f76c990cccf156c0d2910feb9e96ef5a80c3cb7b
vladimirlebedev2000 <50169064+vladimirlebedev2000@users.noreply.github.com>
2021-10-15 13:11:09 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "BuddyAllocator.h"
+using namespace std;
+
+int main() {
+	auto alloc = BuddyAllocator(1<<26);
+	char* p = alloc.allocate(777);
+	alloc.free(p);
+	char* p1 = alloc.allocate(111);
+	alloc.free(p1);
+}
